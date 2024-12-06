04382020e483f04d646ba51aa145943c7fdb32a0
crazyygirl <mounimounika7981@gmail.com>
2024-12-06 11:58:54 +0530

    added code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Added some linegit")
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+isPrime(2);
+function evenOdd(num) {
+    return num % 2 === 0;
+}
+
+evenOdd(3);// adding changes of tracking , add and push
+cd 
